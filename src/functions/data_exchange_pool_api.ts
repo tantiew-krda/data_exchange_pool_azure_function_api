17b238ba5205ffee4d10f931299d2ebe46999e2f
tantiew-krda <tantiew.l@kubota.com>
2025-08-26 08:30:10 +0700

    demo api (GET POST)

--- src/functions/data_exchange_pool_api.ts
+++ src/functions/data_exchange_pool_api.ts
@@ -5,19 +5,55 @@ import {
 	InvocationContext,
 } from "@azure/functions";
 
-export async function data_exchange_pool_api(
+export async function test_get(
 	request: HttpRequest,
 	context: InvocationContext
 ): Promise<HttpResponseInit> {
 	context.log(`Http function processed request for url "${request.url}"`);
 
-	const name = request.query.get("name") || (await request.text()) || "world";
+	const name =
+		request.params?.name ||
+		request.query.get("name") ||
+		"world";
 
-	return { body: `Hello, ${name}, Auto Deploy!` };
+	return { body: `Hello, ${name}, Test Get` };
 }
 
-app.http("data_exchange_pool_api", {
-	methods: ["GET", "POST"],
+
+app.http("test_get", {
+	methods: ["GET"],
 	authLevel: "anonymous",
-	handler: data_exchange_pool_api,
+	route: "test_get/{name?}",
+	handler: test_get,
+});
+
+export async function test_post(
+	request: HttpRequest,
+	context: InvocationContext
+): Promise<HttpResponseInit> {
+	context.log(`Http function processed request for url "${request.url}"`);
+
+	// const name = request.query.get("name") || (await request.text()) || "world";
+
+	// return { body: `Hello, ${name}, Test Post` };
+
+	let name: string | undefined;
+
+	try {
+		const body = (await request.json()) as { name?: string };
+		name = body.name;
+	} catch {
+		// fallback if body is not JSON
+		name = undefined;
+	}
+
+	name = request.query.get("name") || name || "world";
+
+	return { body: `Hello, ${name}, Test Post` };
+}
+
+app.http("test_post", {
+	methods: ["POST"],
+	authLevel: "anonymous",
+	handler: test_post,
 });
